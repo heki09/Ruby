949da8ef4eb082d3f847ab7e18fcbc89033c5041
hekitter <heki09@gmail.com>
2018-11-18 01:57:14 +0900

    basic test

---
+++ basic_test.rb
@@ -0,0 +1,58 @@
+# variable
+greeting = 'Hello!'
+
+puts "#{greeting}Ruby"
+
+puts greeting
+
+# IF sentence
+flag = true
+if flag == true then
+  puts "true"
+end
+
+if flag then
+  puts "true"
+end
+
+flag2 = 0
+if flag2 then
+  puts "true"
+else
+  puts "false"
+end
+
+flag3 = false
+if flag3 then
+  puts "true"
+else
+  puts "false"
+end
+
+flag4 = nil
+if flag4 then
+  puts "true"
+else
+  puts "false"
+end
+
+## while
+i = 1
+while i < 5 do
+  puts i
+  i += 1
+end
+
+puts i
+
+k = 1
+j = 11
+
+while k < 5 do
+  while j< 13 do
+    puts "j= #{j}"
+    j += 1
+  end
+  puts "k= #{k}"
+  k += 1
+end
